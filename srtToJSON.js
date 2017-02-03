6793e2f0119b59ab0ec8a088eb352329e7518424
Gabriel <gabriel@multiverso.me>
2017-02-02 23:31:50 -0200

    Little code cleanup

--- srtToJSON.js
+++ srtToJSON.js
@@ -1,62 +1,51 @@
 /**********************************************************
  * @author Gabriel
- * @version 0.0.1
+ * @version 0.0.2
  * @license DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  **********************************************************
  * @param {string} content The .srt file content
  * @returns {string} The SRT file content converted to JSON
  *********************************************************/
-var srtToJSON = function(content)
-{
-  var quotesArray = [];
+(function() {
+  window.srtToJSON = parseSRT;
+  function parseSRT(content) {
+    var quotesArray = [];
+    var lines = content.split('\r\n');
+    var currentQuoteNumber = 0;
+    var lineTypes = {
+      INDEX : 0,
+      TIMECODE: 1,
+      TEXT: 2
+    };
+    var nextLineType = lineTypes.INDEX;
 
-  var lines = content.split('\r\n');
 
-  var currentQuoteNumber = 0;
+    for(var n in lines) {
+      var line = lines[n].toString();
+      var quoteNumber = parseInt(line, 10);
 
-  var lineTypes =
-  {
-    INDEX : 0,
-    TIMECODE: 1,
-    TEXT: 2
-  }
-
-  var nextLineType = lineTypes.INDEX;
-
-  for(var n in lines)
-  {
-    var line = lines[n].toString();
-    var qN = parseInt(line, 10);
-
-    if(line == '')
-    {
-      nextLineType = lineTypes.INDEX;
-    }
-    else if(nextLineType == lineTypes.INDEX && qN !== NaN)
-    {
-        currentQuoteNumber = qN;
-        var quote = {
-          start_time  : '',
-          end_time    : '',
-          text        : ''
-        };
-        quotesArray[currentQuoteNumber] = quote;
-        nextLineType = lineTypes.TIMECODE;
-    }
-    else if (nextLineType == lineTypes.TIMECODE)
-    {
-      var timeCode = line.split(' --> ');
-      quotesArray[currentQuoteNumber].start_time  = timeCode[0];
-      quotesArray[currentQuoteNumber].end_time    = timeCode[1];
-      nextLineType = lineTypes.TEXT;
-    }
-    else if (nextLineType == lineTypes.TEXT)
-    {
-      if(quotesArray[currentQuoteNumber].text != '') {
-        quotesArray[currentQuoteNumber].text += '\n';
+      if (line == '') nextLineType = lineTypes.INDEX;
+      else if (nextLineType == lineTypes.INDEX && quoteNumber !== NaN) {
+          currentQuoteNumber = quoteNumber;
+          var quote = {
+            start_time  : '',
+            end_time    : '',
+            text        : ''
+          };
+          quotesArray[currentQuoteNumber] = quote;
+          nextLineType = lineTypes.TIMECODE;
+      } else if (nextLineType == lineTypes.TIMECODE) {
+        var timeCode = line.split(' --> ');
+        quotesArray[currentQuoteNumber].start_time  = timeCode[0];
+        quotesArray[currentQuoteNumber].end_time    = timeCode[1];
+        nextLineType = lineTypes.TEXT;
+      } else if (nextLineType == lineTypes.TEXT) {
+        if (quotesArray[currentQuoteNumber].text != '') {
+          quotesArray[currentQuoteNumber].text += '\n';
+        }
+        quotesArray[currentQuoteNumber].text += line;
       }
-      quotesArray[currentQuoteNumber].text += line;
     }
+    return JSON.stringify(quotesArray.filter(function(q){return (q != null);}));
   }
-  return JSON.stringify(quotesArray.filter(function(q){return (q != null);}));
-}
+})();
